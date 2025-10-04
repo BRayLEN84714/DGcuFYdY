9b284c824b11627de64bc154e4981237d5417cf8
BRayLEN84714 <corn-second-04@icloud.com>
2025-10-05 02:18:22 +0800

    Create 设备固件更新 using PYQT

---
+++ firmware_updater_1005_0218_ung.py
@@ -0,0 +1,7 @@
+# 代码生成时间: 2025-10-05 02:18:21
+import sys
+import os
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel, QProgressBar, QMessageBox
+from PyQt5.QtCore import Qt
+
+"
