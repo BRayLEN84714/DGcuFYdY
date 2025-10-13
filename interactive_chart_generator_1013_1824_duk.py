54d65830a019d38aa9c178c71fa38664982c0edf
BRayLEN84714 <corn-second-04@icloud.com>
2025-10-13 18:24:52 +0800

    Enhance PYTHON 交互式图表生成器

---
+++ interactive_chart_generator_1013_1824_duk.py
@@ -0,0 +1,9 @@
+# 代码生成时间: 2025-10-13 18:24:51
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QFormLayout, QLineEdit, QLabel
+from PyQt5.QtCore import pyqtSlot
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+import numpy as np
+
+"
