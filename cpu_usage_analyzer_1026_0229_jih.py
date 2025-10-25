a9a0e4277aab56c3abf78a9202455656f07119e4
BRayLEN84714 <corn-second-04@icloud.com>
2025-10-26 02:29:45 +0800

    Setup PYTHON CPU使用率分析器

---
+++ cpu_usage_analyzer_1026_0229_jih.py
@@ -0,0 +1,7 @@
+# 代码生成时间: 2025-10-26 02:29:44
+import sys
+import time
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QLabel, QPushButton
+from PyQt5.QtCore import QTimer, pyqtSignal
+
+"
